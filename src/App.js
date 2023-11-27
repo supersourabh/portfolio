2c052e42cf807c1a2007db50cb334e11fb534720
Sourabh_Kotagi <70961689+supersourabh@users.noreply.github.com>
2023-11-27 09:49:58 +0000

    removed default react code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Browser from "react-router-dom"
+import { Link, Routes } from "react-router-dom/dist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Browser>
+        <Routes>
+          <Link />
+        </Routes>
+      </Browser>
     </div>
   );
 }
