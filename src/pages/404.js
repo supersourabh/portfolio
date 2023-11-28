6e92aae342cf9a3ca313b77c47e08c282ff9a421
Sourabh_Kotagi <70961689+supersourabh@users.noreply.github.com>
2023-11-28 07:45:23 +0000

    added nevbar

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
-    return (
-        <>
-            <div className='flex '>
-                404 Not Found
-            </div>
-        </>)
+  console.log(process.env);
+  return (
+    <>
+      <div className="flex w-100 item-center justify-center border-b-2 border-gray-300 p-5 text-xl font-bold text-gray-500 tracking-wide">
+        404 Not Found
+      </div>
+      <div className="flex item-center justify-center text-xs font-bold text-red-300 pt-3">
+        <span className="text-gray-300">Powered by </span>{" "}
+        <span className="pl-2"> {process.env.REACT_APP_COMPANY} </span>
+        <span className="text-gray-300 pl-2">
+          v{process.env.REACT_APP_VERSION}
+        </span>
+      </div>
+    </>
+  );
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import skills from "./skills";
 import NotFound from "./404";
+import Home from "./Home";
+import HeaderComp from "../components/HeaderComp";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Hello world</div>
       <Routes>
-        <Route path="/skill" Component={skills} ></Route>
-        <Route path="/*" Component={NotFound} ></Route>
+        {/* <Route path="/" Component={Home}></Route> */}
+
+        <Route Component={HeaderComp}>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/skill" Component={skills}></Route>
+        </Route>
+        <Route path="/*" Component={NotFound}></Route>
       </Routes>
     </BrowserRouter>
   );
-
 }
 export default App;
