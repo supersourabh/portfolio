3abc7984737ece5bb479dfb0d5493a335c54719b
kotagisourabh81 <kotagisourabh81@gmail.com>
2023-11-28 10:37:09 +0530

    added skill route (working)

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom/dist";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import skills from "./skills";
+import NotFound from "./404";
 
 function App() {
   return (
     <BrowserRouter>
       <div>Hello world</div>
       <Routes>
-
-        <Route path="/a" component={skills} ></Route>
+        <Route path="/skill" Component={skills} ></Route>
+        <Route path="/*" Component={NotFound} ></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,6 +1,7 @@
-export default function skills()
-{
-    return <>
-    <div>Hello world from skill</div>
-    </>
+import React from "react";
+
+export default function skills() {
+    return (<>
+        <div>Hello world from skill</div>
+    </>);
 }
