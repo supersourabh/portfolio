29abe13549d44a2929cfe446d78160f0d784e02b
kotagisourabh81 <kotagisourabh81@gmail.com>
2023-11-29 21:45:54 +0530

    added section 1

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Lottie from "lottie-react";
+import animationData from "../assets/Animation-404.json"
 
 export default function NotFound() {
-  console.log(process.env);
+
   return (
     <>
-      <div className="flex w-100 item-center justify-center border-b-2 border-gray-300 p-5 text-xl font-bold text-gray-500 tracking-wide">
-        404 Not Found
+      <div className="flex flex-col w-100 items-center justify-center border-gray-300 p-5 text-4xl font-bold text-gray-500 tracking-wide ">
+        <div className="w-52 h-52">
+          <Lottie animationData={ animationData } loop autoPlay />
+        </div>
+        Not Found
       </div>
       <div className="flex item-center justify-center text-xs font-bold text-red-300 pt-3">
-        <span className="text-gray-300">Powered by </span>{" "}
-        <span className="pl-2"> {process.env.REACT_APP_COMPANY} </span>
+        <span className="text-gray-300">Powered by </span>{ " " }
+        <span className="pl-2"> { process.env.REACT_APP_COMPANY } </span>
         <span className="text-gray-300 pl-2">
-          v{process.env.REACT_APP_VERSION}
+          v{ process.env.REACT_APP_VERSION }
         </span>
       </div>
     </>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../index.css"
 import skills from "./skills";
 import NotFound from "./404";
 import Home from "./Home";
@@ -6,17 +7,19 @@ import HeaderComp from "../components/HeaderComp";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" Component={Home}></Route> */}
+    <div className="h-full m-2 lg:m-6">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" Component={Home}></Route> */ }
 
-        <Route Component={HeaderComp}>
-          <Route path="/" Component={Home}></Route>
-          <Route path="/skill" Component={skills}></Route>
-        </Route>
-        <Route path="/*" Component={NotFound}></Route>
-      </Routes>
-    </BrowserRouter>
+          <Route Component={ HeaderComp }>
+            <Route path="/" Component={ Home }></Route>
+            <Route path="/skill" Component={ skills }></Route>
+          <Route path="/*" Component={ NotFound }></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 export default App;
