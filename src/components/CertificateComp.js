21af955ef2a7a0c35e41e659c6fac23a854513de
kotagisourabh81 <kotagisourabh81@gmail.com>
2023-12-14 10:30:37 +0530

    added all crets

--- src/components/CertificateComp.js
+++ src/components/CertificateComp.js
@@ -2,18 +2,45 @@ import React from "react";
 import certs from "../assets/data/certificateData.json";
 
 export default function CertificateComp() {
+  const color = (opacity = true) => {
+    let colorString = "0123456789ABCDEF";
+    let colorCode = "#";
+    for (let i = 0; i < 6; i++) {
+      colorCode += colorString[Math.floor(Math.random() * 16)];
+    }
+    return opacity ? colorCode + "22" : colorCode;
+  };
   return (
     <div>
       <div className="text-2xl font-bold w-full">Certificates</div>
-      <div className="flex overflow-auto">
+      <div className="flex overflow-auto flex-wrap">
         {certs.map((cert, key) => (
-            <div>
-                <img src={cert.certificate} alt={cert.name} />
-                <div>
-                    
-                </div>
+          <div className="relative w-auto max-w-xs h-60 m-2 group overflow-hidden">
+            <div className="flex w-full h-auto flex-col transition duration-500 group-hover:translate-y-full  origin-bottom z-10 bg-white">
+              <img
+                className="object-contain w-auto h-auto z-10 group-hover:z-10 bg-white"
+                src={cert.certificate}
+                alt={cert.name}
+              />
             </div>
-
+            <div className={` absolute top-0 flex flex-col bg-[${color(true)}] w-full h-full p-2 z-0`} >
+            {/* <img
+                className="object-contain w-full h-auto "
+                src={cert.certificate}
+                alt={cert.name}
+              /> */}
+              <strong className="text-2xl z-0">{cert.name}</strong>
+              <strong className="text-lg text-gray-500 z-0">{cert.category}</strong>
+              <strong className="text-md font-thin z-0">
+                <span>Provider:</span>
+                <span
+                  className={`py-1 px-2 bg-[${color()}] rounded-full text-[${color}] font-semibold`}
+                >
+                  {cert.provider}
+                </span>
+              </strong>
+            </div>
+          </div>
         ))}
       </div>
     </div>
--- src/components/HobbiesComp.js
+++ src/components/HobbiesComp.js
@@ -10,10 +10,12 @@ export default function HobbiesComp() {
           {hobbies.map((hobbie, key) => (
             <li className="group relative w-auto h-48 m-2 lg:mr-6 overflow-hidden">
               <div className="absolute flex justify-center items-center w-full h-full p-5 t">
-                <p className="text-xl text-white z-10 font-bold group-hover:translate-y-14 ransition duration-300 ease-in-out group-hover:text-gray-400">{hobbie.name}</p>
+                <p className="text-xl text-white z-10 font-bold group-hover:translate-y-14 ransition duration-300 ease-in-out group-hover:text-gray-400">
+                  {hobbie.name}
+                </p>
               </div>
               <img
-                className="object-contain w-auto h-48 z-0 blur-[3px] transition ease-in-out duration-500 hover:blur-none origin-bottom hover:skew-y-0 "
+                className="object-contain w-auto h-48 z-0 blur-[3px] transition ease-in-out duration-500 hover:blur-none origin-bottom"
                 src={hobbie.image}
                 alt={hobbie.name}
               />
