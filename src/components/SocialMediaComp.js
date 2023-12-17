49c114ef1a7aa871f3f2cf3bf39a47ba74f9d4c1
kotagisourabh81 <kotagisourabh81@gmail.com>
2023-12-17 21:36:25 +0530

    sidebar styling

--- src/components/SocialMediaComp.js
+++ src/components/SocialMediaComp.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import github from "../assets/social_media/github.svg";
 import linkedin from "../assets/social_media/linkedin.svg";
 import instagram from "../assets/social_media/instagram.svg";
 import whatsapp from "../assets/social_media/whatsapp.svg";
 import mail from "../assets/social_media/mail.svg";
 import mobile from "../assets/social_media/mobile.svg";
+import menu from "../assets/social_media/menu.svg";
 
 export default function SocialMediaComp() {
   let shadow = { filter: `drop-shadow(0 0 1rem  #DAC8AE9f)` };
+  const [openedDropdown, setOpenedDropdown] = useState(false);
   let iconsData = [
     {
       name: "GitHub",
@@ -41,11 +43,24 @@ export default function SocialMediaComp() {
     },
   ];
   return (
-    <section className="w-full lg:w-1/4 text-white flex-0.5 bg-gray-700 shadow-lg lg:mt-5 rounded-lg  p-2 h-max z-0">
-      <ul className="text-lg font-semibold tracking-wide text-gray-300">
+    <section className="w-full lg:w-1/4 text-white flex-0.5 bg-gray-700 shadow-lg lg:mt-5 rounded-lg  p-2 h-max z-0 lg:sticky top-28">
+      <div
+        onClick={() => setOpenedDropdown(!openedDropdown)}
+        className="flex w-full px-3 py-2 lg:hidden"
+      >
+        <img className="w-10" src={menu} alt="menu" />
+        <strong className="flex items-center mx-auto text-2xl text-white ">
+          Social Media
+        </strong>
+      </div>
+      <ul
+        className={`text-lg font-semibold tracking-wide text-gray-300 overflow-hidden transition duration-300 lg:h-auto  ${
+          openedDropdown ? "h-auto translate-y-3" : "h-0 translate-y-0 "
+        } lg:block `}
+      >
         <li
           title="section-left-header"
-          className="flex items-center px-3 lg:px-5 py-5 text-2xl text-white"
+          className="flex items-center px-3 lg:px-5 py-5 text-2xl text-white hidden lg:block"
         >
           Social Media
         </li>
